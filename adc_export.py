128ae02e4f688a0cf57ebc1ae2e0b9851693a969
sa-ssatyakam <sa-ssatyakam@bmi-util-0.kumc.edu>
2023-04-26 10:14:02 -0500

    testing the data causing the export issue

--- adc_export.py
+++ adc_export.py
@@ -59,6 +59,8 @@ def main(get_config,
                 if data is None:
                     break
                 # remove the header of the CSV
+                log.info('data is: %s, form:%s ',
+                     data)
                 data = data.split('\n', 1)[1] if header_written else data
                 op_file.write(data.encode('utf-8'))
                 header_written = True
