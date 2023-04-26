6bde9f88ac6bf8443eef6ffd5ee84c7ab51bacbd
siddharthsatyakam <43289998+siddharthsatyakam@users.noreply.github.com>
2023-04-26 13:16:57 -0500

    7089 header issue adc export (#23)
    * testing the data causing the export issue
    
    * as the issue returned by export records is related to "field_names"
    
    * implementing the fix of fields param mantioned only when there are specific fields
    
    * correcting all the issues as pointed in pull request 23
    
    ---------
    
    Co-authored-by: sa-ssatyakam <sa-ssatyakam@bmi-util-0.kumc.edu>

--- adc_export.py
+++ adc_export.py
@@ -54,7 +54,6 @@ def main(get_config,
                 data = data_proj.export_records(records=record_chunk,
                                                 format=file_format,
                                                 forms=[row['formname'], ],
-                                                fields=field_names,
                                                 event_name='unique')
                 if data is None:
                     break
